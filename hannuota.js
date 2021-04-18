c17fab4326c5a6fae5570728ac02156762235a49
aa <hapi@hapi.com>
2021-04-19 02:31:31 +0800

    refactor version2

--- hannuota.js
+++ hannuota.js
@@ -1,59 +1,92 @@
-const a = 7 // 5个
-// const arr1 = [1, 2, 3, 4, 5, 6, 7]
-const arr1 = []
-for (let i = 0; i < a; i++) {
-  arr1[i] = n++
-}
-const arr2 = []
-const arr3 = []
-const container = [arr1, arr2, arr3]
-let i = -1 // 接受过人的数组下标 which accepted last time
-let canReceive = null
-let canOut = null
-let temporary1 = null
-let temporary2 = null
-let count = 0 // 次数
-while (arr3.length !== a) {
-  if (i < 0) {
-    const one = arr1.shift(arr1[0])
-    if (a % 2 === 1) {
-      arr3.unshift(one)
-      i = arr3
-    } else {
-      arr2.unshift(one)
-      i = arr2
-    }
+const num = 5 // 5个
+console.log('count: ' + hannuota(num)) // 输出
+
+function hannuota (num) {
+  // 生成3个柱子
+  const arr1 = [] // const arr1 = [1, 2, 3, 4, 5, 6, 7]
+  for (let i = 1; i <= num; i++) {
+    arr1.push(i)
+  }
+  const arr2 = []
+  const arr3 = []
+  const container = [arr1, arr2, arr3] // 底座
+  let lastReceive = null // 接受过人的数组 which accepted last time
+  let count = 0 // 次数
+
+  // 第一次
+  const one = arr1.shift(arr1[0])
+  if (num % 2 === 1) { // 奇3偶2
+    arr3.unshift(one)
+    lastReceive = arr3
   } else {
-    canOut = filter(i).find((arr, index) => {
-      canReceive = filter(arr).find(item => {
-        if (item.length === 0) {
-          temporary1 = item
-          return
+    arr2.unshift(one)
+    lastReceive = arr2
+  }
+  count++
+
+  // 第2到 n次
+  while (arr3.length !== num) {
+    let canReceive
+    let canOut
+    // 获取其他成员
+    const exporters = otherMember(lastReceive, container)
+
+    // 获取适合输出
+    canOut = exporters.reduce((exporter1, exporter2) => {
+      // 排除空的之后，小的输出
+      if (exporter1.length !== 0) {
+        if (exporter2.length !== 0) {
+          return exporter1[0] > exporter2[0] ? exporter2 : exporter1
         }
 
-        return item[0] > arr[0] && arr[0] % 2 !== item[0] % 2
-      })
-
-      !canReceive && temporary1 && (canReceive = temporary1)
-      index === 0 && (temporary2 = arr)
-      if (!canReceive) return false
-      if (canReceive.length === 0) return true
-      return canReceive[0] > arr[0] && arr[0] % 2 !== canReceive[0] % 2
+        return exporter1
+      } else {
+        return exporter2
+      }
     })
 
-    !canOut && temporary2 && (canOut = temporary2)
-    const one = canOut.shift(canOut[0])
+    // 获取其他成员
+    const recipients = otherMember(canOut, container)
+
+    // 比较适合接收
+    canReceive = recipients.reduce((recipient1, recipient2) => {
+      stateP1 = isPartner(recipient1, canOut) // boolean
+      stateP2 = isPartner(recipient2, canOut) // boolean
+      /*               can in?
+       *  p1.length  p1      p2
+       *  0          true  false   1
+       *  0          true   true   2
+       *  1          true   true   1
+       *  1          false  true   2
+       *  1          false  false  n
+       *  1          true   false  1
+       */
+      // 都为true的时候选第二个， 其他时候谁行谁放
+      if (stateP1 && stateP2 && recipient1.length === 0) { // 同时都可以接收的情况
+        return recipient2
+      }
+
+      // 优先放recipient1   stateP1 && stateP2 && recipient1.length !== 0
+      if (stateP1) return recipient1
+      if (stateP2) return recipient2
+
+    })
+    // 移动
+    const one = canOut.shift()
     canReceive.unshift(one)
-    i = canReceive
-    canReceive = null
-    canOut = null
-    temporary1 = null
-    temporary2 = null
+    lastReceive = canReceive
+    ++count
   }
-  ++count
+
+  return count
 }
 
-function filter(val) {
-  // 过滤掉不合条件的数组
-  return container.filter(arr => arr !== val)
+function otherMember (me, team) {
+  // 取反
+  return team.filter(member => member !== me)
+}
+
+function isPartner ([receive], [out]) {
+  if (receive === undefined) return true // 空的能进
+  return receive > out && out % 2 !== receive % 2 // 下>上 && 奇偶错开
 }
